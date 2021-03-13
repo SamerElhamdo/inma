730ec2d79457ea744b84d40dd97ce99a6368fba1
samerelhamdo <samer.elhamdoo@gmail.com>
2021-03-13 03:15:42 +0200

    add our prouduct

--- static/js/main.js
+++ static/js/main.js
@@ -2,4 +2,15 @@ $( document ).ready(function() {
     $('.search-btn').on('click', function(){
         $('.search-input').toogleClass('active');
     });
+
+    $('.toggle-detail').on('click', function(){
+    	console.log('hi there')
+    	$(this).next().toggleClass('active');
+
+    });
+    $('.close-detail').on('click', function(){
+    	console.log('hi there')
+    	$(this).parent().toggleClass('active');
+
+    });
 });
