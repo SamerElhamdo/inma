e65008e99303d676f2554b2ec54feb7e6e8e8e07
samerelhamdo <samer.elhamdoo@gmail.com>
2021-03-15 19:24:31 +0200

    add more product and add anmate scroll and conect github with heroku

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,19 @@
 $( document ).ready(function() {
+
+    var html = $("html");
+
+    $('.us-products-button').on('click', function(event){
+            event.preventDefault();
+            html.animate({scrollTop: $("#our_products").offset().top - 30}, '800');
+    });
+
+    $('.contact_us_button').on('click', function(event){
+            event.preventDefault();
+            html.animate({scrollTop: $("#contact_us").offset().top - 30}, '800');
+    });
+    
+    contact_us
+
     $('.search-btn').on('click', function(){
         $('.search-input').toogleClass('active');
     });
